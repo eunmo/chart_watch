83f91f9c6e28ea239e60d93c6ea50f276bb77c50
eunmo <eunmo@cs.stanford.edu>
2015-01-12 15:31:24 +0000

    added filter and table to front end

---
+++ client/js/filter.js
@@ -0,0 +1,18 @@
+musicApp.filter('songDuration', function () {
+	return function(secs) {
+		var hour = Math.floor(secs / 3600);
+		var min = Math.floor(secs / 60);
+		var sec = secs % 60;
+		var str = (hour > 0 ? (hour + ':') : '') +
+							(min >= 10 ? '' : '0') + min + ':' +
+							(sec >= 10 ? '' : '0') + sec;
+
+		return str;
+	};
+});
+
+musicApp.filter('normalizeString', function () {
+	return function(str) {
+		return str.replace(/`/g, '\'');
+	};
+});
