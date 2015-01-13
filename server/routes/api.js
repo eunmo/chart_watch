20c8339023af5808f6cbf92d820d51b4680a37d1
eunmo <eunmo@cs.stanford.edu>
2015-01-13 15:37:23 +0000

    api for sorted list of artists

--- server/routes/api.js
+++ server/routes/api.js
@@ -7,5 +7,11 @@
 				res.json(songs);
 			});
 		});
+		
+		router.get('/api/artist', function(req, res) {
+			models.Artist.findAll({order: '`nameNorm`'}).then(function(artists) {
+				res.json(artists);
+			});
+		});
 	};
 }());
