2d6e1b233448676b367c9a255d3a652b05d5120a
eunmo <eunmo@cs.stanford.edu>
2015-03-05 13:54:13 +0000

    add perl script for Melon chart

--- perl/melon.pl
+++ perl/melon.pl
@@ -4,22 +4,22 @@ use utf8;
 use Encode;
 use Mojo::DOM;
 use Mojo::Collection;
+use DateTime;
 binmode(STDOUT, ":utf8");
 
 my $week = $ARGV[0];
 my $year = $ARGV[1];
-my $week_string = sprintf("%02d", $week);
 
-my $url = "http://www.gaonchart.co.kr/main/section/chart/online.gaon?nationGbn=T&serviceGbn=ALL&targetTime=$week_string&hitYear=$year&termGbn=week";
+my $url = get_url();
 my $html = get("$url");
 my $dom = Mojo::DOM->new($html);
 my $rank = 1;
 
 print "[";
 
-for my $div ($dom->find('td[class*="subject"]')->each) {
-	my $title = $div->find('p')->first->text;
-	my $artist = $div->find('p[class~="singer"]')->first->attr('title');
+for my $div ($dom->find('div[class*="wrap_song_info"]')->each) {
+	my $title = $div->find('a')->first->text;
+	my $artist = $div->find('div[class~="rank02"]')->first->find('a')->first->text;
 	my $title_norm = normalize_title($title);
 	my $artist_norm = normalize_artist($artist);
 	print ",\n" if $rank > 1;
@@ -28,6 +28,66 @@ for my $div ($dom->find('td[class*="subject"]')->each) {
 }
 
 print "]";
+
+sub first_day_of_week_old
+{
+	my ($year, $week) = @_;
+
+	DateTime->new( year => $year, month => 1, day => 1 )
+					->add( weeks => ($week - 1) )
+					->truncate( to => 'week' )
+					->subtract( days => 1);
+}
+
+sub last_day_of_week_old
+{
+	my ($year, $week) = @_;
+
+	DateTime->new( year => $year, month => 1, day => 1 )
+					->add( weeks => ($week - 1) )
+					->truncate( to => 'week' )
+					->add( days => 5 );
+}
+
+sub first_day_of_week_new
+{
+	my ($year, $week) = @_;
+
+	DateTime->new( year => $year, month => 1, day => 1 )
+					->add( weeks => ($week - 1) )
+					->truncate( to => 'week' );
+}
+
+sub last_day_of_week_new
+{
+	my ($year, $week) = @_;
+
+	DateTime->new( year => $year, month => 1, day => 1 )
+					->add( weeks => ($week - 1) )
+					->truncate( to => 'week' )
+					->add( days => 6 );
+}
+
+sub get_url
+{
+	my $age = int($year / 10) * 10;
+	my $fd, $ld;
+
+	if ($year < 2012 || ($year == 2012 && $week <= 33)) {
+		$fd = first_day_of_week_old($year, $week);
+		$ld = last_day_of_week_old($year, $week);
+	} else {
+		$fd = first_day_of_week_new($year, $week);
+		$ld = last_day_of_week_new($year, $week);
+	}
+
+	my $fd_ymd = $fd->ymd('');
+	my $ld_ymd = $ld->ymd('');
+	my $url = sprintf("http://www.melon.com/chart/search/list.htm?chartType=WE&age=%d&year=%d&mon=%02d&day=%d%%5E%d&classCd=DP0000&startDay=%d&endDay=%d&moved=Y", $age, $year, $ld->month(), $fd_ymd, $ld_ymd, $fd_ymd, $ld_ymd);
+	print $url;
+
+	return $url;
+}
 
 sub normalize_title($)
 {
@@ -64,6 +124,7 @@ sub normalize_artist($)
 	if ($string =~ /^슈퍼주니어 예성/) { return "예성"; }
 	if ($string =~ /^오렌지캬라멜/) { return "오렌지 캬라멜"; }
 	if ($string =~ /^울랄라세션/) { return "울랄라 세션"; }
+	if ($string =~ /^윤미래/) { return "윤미래"; }
 	if ($string =~ /^존 박/) { return "존박"; }
 	if ($string =~ /^소녀시대-태티서/) { return "태티서"; }
 	if ($string =~ /^포미닛/) { return "4minute"; }
