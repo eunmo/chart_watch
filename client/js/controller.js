77da5d65e0d46f1b0ef1e8d82f0b9e41a3f80c68
eunmo <eunmo@cs.stanford.edu>
2015-10-06 12:15:52 +0000

    display chart name

--- client/js/controller.js
+++ client/js/controller.js
@@ -417,6 +417,13 @@ musicApp.controller('ChartCtrl', function ($rootScope, $scope, $routeParams, $ht
 		}
 	}
 
+	function capitalize(string) {
+		if (string.length < 3)
+			return string.toUpperCase();
+
+		return string.charAt(0).toUpperCase() + string.slice(1);
+	}
+
 	$scope.adjustDate = function () {
 		var time = $scope.date.getTime();
 		if ($scope.max < time) {
@@ -429,6 +436,7 @@ musicApp.controller('ChartCtrl', function ($rootScope, $scope, $routeParams, $ht
 	};
 
 	$scope.chart = $routeParams.name;
+	$scope.chartName = capitalize($scope.chart);
 	$scope.maxDate = getMaxDate($scope.chart);
 	$scope.minDate = getMinDate($scope.chart);
 	
