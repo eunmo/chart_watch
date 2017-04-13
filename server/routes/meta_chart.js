cc71dc643dc812fe92dc52b694ec70e923a2f62f
eunmo <eunmo@cs.stanford.edu>
2017-04-13 22:16:25 +0900

    add partial albums to missing albums view

--- server/routes/meta_chart.js
+++ server/routes/meta_chart.js
@@ -382,6 +382,19 @@
 
 		router.get('/chart/missing/album/:_rank', function (req, res) {
 			var queryString =
+				"(SELECT name, rank, a.title, count, chart, week, note FROM " +
+				 "(SELECT artist as name, min(rank) as rank, title, count(*) as count, min(type) as chart, min(week) as week " + 
+				    "FROM AlbumCharts where rank <= " + req.params._rank +
+				    " AND AlbumId is null " +
+				   "GROUP BY artist, title) a " +
+				 "LEFT JOIN AlbumChartNotes b ON (a.name = b.artist AND a.title = b.title)) " +
+				"UNION " +
+				"(SELECT artist as name, min(rank) as rank, title, count(*) as count, min(type) as chart, min(week) as week, 'Partial' " +
+				   "FROM AlbumCharts where rank <= " + req.params._rank +
+					 " AND AlbumId in (SELECT id from Albums WHERE format = 'Partial') " +
+					"GROUP BY artist, title) " +
+				"ORDER BY week, chart;";
+				/*
 				"SELECT name, rank, a.title, count, chart, week, note FROM " +
 				"(SELECT artist as name, min(rank) as rank, title, count(*) as count, min(type) as chart, min(week) as week " + 
 				 "FROM AlbumCharts where rank <= " + req.params._rank +
@@ -389,6 +402,7 @@
 				 "GROUP BY artist, title " +
 				 "ORDER BY week, chart) a " +
 				"LEFT JOIN AlbumChartNotes b ON (a.name = b.artist AND a.title = b.title) ORDER BY week, chart;";
+				*/
 			models.sequelize.query(queryString, { type: models.sequelize.QueryTypes.SELECT })
 			.then(function (rows) {
 				res.json(rows);
