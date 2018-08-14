a15cc5f2dbb95ba8e97e38c7be95d3c0868d469f
eunmo <eunmo@cs.stanford.edu>
2018-08-14 17:31:22 +0900

    display artist Bs

--- src/components/NameArray/index.js
+++ src/components/NameArray/index.js
@@ -14,6 +14,38 @@ export default class NameArray extends Component {
 			if (index > 0)
 				string += ', ';
 			string += TextUtil.normalize(artist.name);
+
+			if (artist.Bs) {
+				var Bs = [];
+				var type = '';
+				// display precedence -> a, p, c, m, u, f
+				if (artist.Bs.a) {
+					Bs = [artist.Bs.a];
+					type = 'aka ';
+				} else if (artist.Bs.p) {
+					Bs = artist.Bs.p;
+				} else if (artist.Bs.c) {
+					Bs = [artist.Bs.c];
+					type = 'cv ';
+				} else if (artist.Bs.m) {
+					Bs = [artist.Bs.m];
+				} else if (artist.Bs.u) {
+					Bs = [artist.Bs.u];
+				} else if (artist.Bs.f) {
+					Bs = [artist.Bs.f];
+				}
+
+				if (Bs.length > 0) {
+					string += ' (' + type;
+						Bs.forEach((artist, index) => {
+							if (index > 0)
+								string += '+';
+
+							string += TextUtil.normalize(artist.name);
+						});
+					string += ')';
+				}
+			}
 		});
 
 		return <span>{string}</span>;
