6cbf034375f363f29121c51915c622767765c52a
eunmo <eunmo@cs.stanford.edu>
2018-08-15 02:58:14 +0900

    add relations tab to artist component

--- src/components/Artist/index.js
+++ src/components/Artist/index.js
@@ -56,6 +56,8 @@ export default class Artist extends Component {
 			views.push({name: 'Songs', view: this.getSongsView(songs)});
 		if (features.length > 0)
 			views.push({name: 'Features', view: features.map(this.getFeatureView)});
+		if (artist.As.length > 0 || artist.Bs !== undefined)
+			views.push({name: 'Relations', view: this.getRelationsView()});
 
 		return (
 			<div>
@@ -75,6 +77,84 @@ export default class Artist extends Component {
 				</div>
 			</div>
 		);
+	}
+
+	getAs() {
+		const artist = this.state.artist;
+		if (artist.As.length === 0)
+			return [];
+
+		var i, A;
+		var As = {};
+
+		for (i in artist.As) {
+			A = artist.As[i];
+			if (As[A.type] === undefined)
+				As[A.type] = [];
+
+			As[A.type].push(A);
+		}
+
+		var array = [];
+		for (i in As) {
+			As[i].sort((a, b) => { var x = a.name; var y = b.name; return ((x < y) ? -1 : ((x > y) ? 1 : 0)); });
+			array.push({ type: i, artists: As[i] });
+		}
+
+		array.sort((a, b) => { var x = a.type; var y = b.type; return ((x < y) ? -1 : ((x > y) ? 1 : 0)); });
+		return array;
+	}
+
+	getBs() {
+		const artist = this.state.artist;
+		if (artist.Bs === undefined)
+      return [];
+
+    var array = [];
+    for (var i in artist.Bs) {
+      if (i === 'p') {
+        array.push({ type: i, artists: artist.Bs[i] });
+      }else {
+        array.push({ type: i, artists: [artist.Bs[i]] });
+      }
+    }
+
+    array.sort((a, b) => { var x = a.type; var y = b.type; return ((x < y) ? -1 : ((x > y) ? 1 : 0)); });
+		return array;
+	}
+
+	getRelationsView() {
+		var As = {
+			a: 'Alias of ',
+			c: 'Voiced Character ',
+			f: 'Had Former Member ',
+			m: 'Has Member ',
+			p: 'In Project Group ',
+			u: 'Has Unit '
+		};
+
+		var Bs = {
+			a: 'Alias of ',
+			c: 'Voiced by ',
+			f: 'Former Member of ',
+			m: 'Member of ',
+			p: 'Project Group of ',
+			u: 'Unit of '
+		};
+
+		var aViews = this.getAs().map(rel => (
+			<div key={'A' + rel.type}>
+				{this.getArtistView(As[rel.type], rel.artists)}
+			</div>
+		));
+		
+		var bViews = this.getBs().map(rel => (
+			<div key={'B' + rel.type}>
+				{this.getArtistView(Bs[rel.type], rel.artists)}
+			</div>
+		));
+		
+		return aViews.concat(bViews);
 	}
 
 	getRankView(song) {
