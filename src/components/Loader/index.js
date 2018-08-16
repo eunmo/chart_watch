1c3c3cfb31aa62e66b1e1462493a430305a80eb6
eunmo <eunmo@cs.stanford.edu>
2018-08-16 13:34:38 +0900

    change class->className in loader

--- src/components/Loader/index.js
+++ src/components/Loader/index.js
@@ -5,9 +5,9 @@ import './style.css';
 export default class Loader extends Component {
 	render() {
 		return (
-			<div class="Spinner">
-  			<div class="Spinner-cube1"></div>
-			  <div class="Spinner-cube2"></div>
+			<div className="Spinner">
+  			<div className="Spinner-cube1"></div>
+			  <div className="Spinner-cube2"></div>
 			</div>
 		);
 	}
