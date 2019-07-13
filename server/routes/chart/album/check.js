ffe46d59abe2df361f18d0f585c1b5482f910ee9
eunmo <eunmo@cs.stanford.edu>
2019-07-13 22:22:55 +0900

    make chart check serialized

--- server/routes/chart/album/check.js
+++ server/routes/chart/album/check.js
@@ -6,89 +6,92 @@ var exec = Promise.promisify(require('child_process').exec);
 const Match = require('../../../util/match_album');
 
 module.exports = function(router, models, db) {
+  let check = async function(chart) {
+    const curWeek = new Date(chart.week);
+    var nextWeek = new Date(chart.week);
+    nextWeek.setDate(nextWeek.getUTCDate() + 7);
+
+    const dateStr = nextWeek
+      .toISOString()
+      .substr(0, 10)
+      .replace(/-/g, ' ');
+    const execStr =
+      'perl ' +
+      path.join(__dirname, '../../../../perl/album', chart.type + '.pl') +
+      ' ' +
+      dateStr;
+
+    const [stdout, stderr] = await exec(execStr);
+    const data = JSON.parse(stdout);
+
+    if (data.length <= 10) return;
+
+    var query =
+      'SELECT `rank`, artist, title ' +
+      '  FROM AlbumCharts ' +
+      " WHERE `type` = '" +
+      chart.type +
+      "' " +
+      "   AND `week` = '" +
+      curWeek.toISOString() +
+      "' " +
+      '	ORDER BY `rank`;';
+
+    const curWeekRows = await db.promisifyQuery(query);
+
+    if (curWeekRows.length !== data.length) return;
+
+    var diffCount = 0;
+
+    data.forEach((row, index) => {
+      const record = curWeekRows[index];
+      if (row.artist !== record.artist || row.title !== record.title)
+        diffCount++;
+    });
+
+    if (diffCount <= 10) return;
+
+    var values = [];
+    data.forEach(row => {
+      values.push(
+        '(DEFAULT,' +
+          "'" +
+          chart.type +
+          "'," +
+          "'" +
+          nextWeek
+            .toISOString()
+            .slice(0, 19)
+            .replace('T', ' ') +
+          "'," +
+          row.rank +
+          ',' +
+          "'" +
+          row.artist +
+          "'," +
+          "'" +
+          row.title +
+          "'," +
+          'curdate(),curdate())'
+      );
+    });
+
+    query =
+      'INSERT INTO `AlbumCharts` ' +
+      '(`id`,`type`,`week`,`rank`,`artist`,`title`,`updatedAt`,`createdAt`) ' +
+      'VALUES ' +
+      values.join(',');
+
+    await db.promisifyQuery(query);
+
+    await Match.matchWeek(models, db, chart.type, nextWeek);
+  };
   router.get('/chart/album/check', async function(req, res) {
     var current = await db.chartCurrent.getCurrentWeeks('AlbumCharts');
 
-    current.forEach(async function(chart) {
-      const curWeek = new Date(chart.week);
-      var nextWeek = new Date(chart.week);
-      nextWeek.setDate(nextWeek.getUTCDate() + 7);
-
-      const dateStr = nextWeek
-        .toISOString()
-        .substr(0, 10)
-        .replace(/-/g, ' ');
-      const execStr =
-        'perl ' +
-        path.join(__dirname, '../../../../perl/album', chart.type + '.pl') +
-        ' ' +
-        dateStr;
-
-      const [stdout, stderr] = await exec(execStr);
-      const data = JSON.parse(stdout);
-
-      if (data.length <= 10) return;
-
-      var query =
-        'SELECT `rank`, artist, title ' +
-        '  FROM AlbumCharts ' +
-        " WHERE `type` = '" +
-        chart.type +
-        "' " +
-        "   AND `week` = '" +
-        curWeek.toISOString() +
-        "' " +
-        '	ORDER BY `rank`;';
-
-      const curWeekRows = await db.promisifyQuery(query);
-
-      if (curWeekRows.length !== data.length) return;
-
-      var diffCount = 0;
-
-      data.forEach((row, index) => {
-        const record = curWeekRows[index];
-        if (row.artist !== record.artist || row.title !== record.title)
-          diffCount++;
-      });
-
-      if (diffCount <= 10) return;
-
-      var values = [];
-      data.forEach(row => {
-        values.push(
-          '(DEFAULT,' +
-            "'" +
-            chart.type +
-            "'," +
-            "'" +
-            nextWeek
-              .toISOString()
-              .slice(0, 19)
-              .replace('T', ' ') +
-            "'," +
-            row.rank +
-            ',' +
-            "'" +
-            row.artist +
-            "'," +
-            "'" +
-            row.title +
-            "'," +
-            'curdate(),curdate())'
-        );
-      });
-
-      query =
-        'INSERT INTO `AlbumCharts` ' +
-        '(`id`,`type`,`week`,`rank`,`artist`,`title`,`updatedAt`,`createdAt`) ' +
-        'VALUES ' +
-        values.join(',');
-
-      await db.promisifyQuery(query);
-
-      await Match.matchWeek(models, db, chart.type, nextWeek);
-    });
+    for (var i = 0; i < current.length; i += 1) {
+      await check(current[i]);
+    }
 
     res.sendStatus(200);
   });
--- server/routes/chart/single/check.js
+++ server/routes/chart/single/check.js
@@ -7,96 +7,100 @@ const Match = require('../../../util/match_single');
 const APN = require('../../../util/apn');
 
 module.exports = function(router, models, db) {
+  let check = async function(chart) {
+    const curWeek = new Date(chart.week);
+    var nextWeek = new Date(chart.week);
+    nextWeek.setDate(nextWeek.getUTCDate() + 7);
+
+    const dateStr = nextWeek
+      .toISOString()
+      .substr(0, 10)
+      .replace(/-/g, ' ');
+    const execStr =
+      'perl ' +
+      path.join(__dirname, '../../../../perl/single', chart.type + '.pl') +
+      ' ' +
+      dateStr;
+
+    const [stdout, stderr] = await exec(execStr);
+    const data = JSON.parse(stdout);
+
+    if (data.length <= 10) return;
+
+    var query =
+      'SELECT `rank`, artist, title ' +
+      '  FROM SingleCharts ' +
+      " WHERE `type` = '" +
+      chart.type +
+      "' " +
+      "   AND `week` = '" +
+      curWeek.toISOString() +
+      "' " +
+      '   AND `order` = 0' +
+      '	ORDER BY `rank`;';
+
+    const curWeekRows = await db.promisifyQuery(query);
+
+    if (curWeekRows.length !== data.length) return;
+
+    var diffCount = 0;
+
+    data.forEach((row, index) => {
+      const record = curWeekRows[index];
+      if (row.artist !== record.artist || row.titles[0] !== record.title)
+        diffCount++;
+    });
+
+    if (diffCount <= 10) return;
+
+    var values = [];
+    data.forEach(row => {
+      for (var i in row.titles) {
+        values.push(
+          '(DEFAULT,' +
+            "'" +
+            chart.type +
+            "'," +
+            "'" +
+            nextWeek
+              .toISOString()
+              .slice(0, 19)
+              .replace('T', ' ') +
+            "'," +
+            row.rank +
+            ',' +
+            i +
+            ',' +
+            "'" +
+            row.artist +
+            "'," +
+            "'" +
+            row.titles[i] +
+            "'," +
+            'curdate(),curdate())'
+        );
+      }
+    });
+
+    query =
+      'INSERT INTO `SingleCharts` ' +
+      '(`id`,`type`,`week`,`rank`,`order`,`artist`,`title`,`updatedAt`,`createdAt`) ' +
+      'VALUES ' +
+      values.join(',');
+
+    await db.promisifyQuery(query);
+
+    await Match.matchWeek(models, db, chart.type, nextWeek);
+
+    APN.notify(chart.type);
+  };
+
   router.get('/chart/single/check', async function(req, res) {
     var current = await db.chartCurrent.getCurrentWeeks('SingleCharts');
 
-    current.forEach(async function(chart) {
-      const curWeek = new Date(chart.week);
-      var nextWeek = new Date(chart.week);
-      nextWeek.setDate(nextWeek.getUTCDate() + 7);
-
-      const dateStr = nextWeek
-        .toISOString()
-        .substr(0, 10)
-        .replace(/-/g, ' ');
-      const execStr =
-        'perl ' +
-        path.join(__dirname, '../../../../perl/single', chart.type + '.pl') +
-        ' ' +
-        dateStr;
-
-      const [stdout, stderr] = await exec(execStr);
-      const data = JSON.parse(stdout);
-
-      if (data.length <= 10) return;
-
-      var query =
-        'SELECT `rank`, artist, title ' +
-        '  FROM SingleCharts ' +
-        " WHERE `type` = '" +
-        chart.type +
-        "' " +
-        "   AND `week` = '" +
-        curWeek.toISOString() +
-        "' " +
-        '   AND `order` = 0' +
-        '	ORDER BY `rank`;';
-
-      const curWeekRows = await db.promisifyQuery(query);
-
-      if (curWeekRows.length !== data.length) return;
-
-      var diffCount = 0;
-
-      data.forEach((row, index) => {
-        const record = curWeekRows[index];
-        if (row.artist !== record.artist || row.titles[0] !== record.title)
-          diffCount++;
-      });
-
-      if (diffCount <= 10) return;
-
-      var values = [];
-      data.forEach(row => {
-        for (var i in row.titles) {
-          values.push(
-            '(DEFAULT,' +
-              "'" +
-              chart.type +
-              "'," +
-              "'" +
-              nextWeek
-                .toISOString()
-                .slice(0, 19)
-                .replace('T', ' ') +
-              "'," +
-              row.rank +
-              ',' +
-              i +
-              ',' +
-              "'" +
-              row.artist +
-              "'," +
-              "'" +
-              row.titles[i] +
-              "'," +
-              'curdate(),curdate())'
-          );
-        }
-      });
-
-      query =
-        'INSERT INTO `SingleCharts` ' +
-        '(`id`,`type`,`week`,`rank`,`order`,`artist`,`title`,`updatedAt`,`createdAt`) ' +
-        'VALUES ' +
-        values.join(',');
-
-      await db.promisifyQuery(query);
-
-      await Match.matchWeek(models, db, chart.type, nextWeek);
-
-      APN.notify(chart.type);
-    });
+    for (var i = 0; i < current.length; i += 1) {
+      await check(current[i]);
+    }
 
     res.sendStatus(200);
   });
